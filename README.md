4057be323ba85022ff83c31f6bed241291b3087f
Jhonatan Eduardo Rampim <jhonatan.rampim@gmail.com>
2020-05-15 15:04:42 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# todo
+
+Aplicativo básico em flutter desenvolvido com base no curso do Balta.io 
+
+Link: https://balta.io/cursos/criando-seu-primeiro-app-com-flutter
+
+## Getting Started
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:todo/models/item.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  var items = new List<Item>();
+  HomePage() {
+    items = [];
+    items.add(Item(title: "Item 1", done: false));
+    items.add(Item(title: "Item 2", done: true));
+    items.add(Item(title: "Item 3", done: false));
+  }
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var newTaskCtrl = TextEditingController();
+
+  void add() {
+    if (newTaskCtrl.text.isEmpty) return;
+
+    setState(() {
+      widget.items.add(
+        Item(title: newTaskCtrl.text, done: false),
+      );
+      newTaskCtrl.text = "";
+    });
+  }
+
+  void remove(int index) {
+    if (newTaskCtrl.text.isEmpty) return;
+
+    setState(() {
+      widget.items.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: TextFormField(
+          controller: newTaskCtrl,
+          keyboardType: TextInputType.text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+          ),
+          decoration: InputDecoration(
+            labelText: "Nova Tarefa",
+            labelStyle: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+      body: ListView.builder(
+          itemCount: widget.items.length,
+          itemBuilder: (BuildContext context, int index) {
+            final item = widget.items[index];
+            return Dismissible(
+              child: CheckboxListTile(
+                title: Text(item.title),
+                value: item.done,
+                onChanged: (value) {
+                  setState(() {
+                    item.done = value;
+                  });
+                },
+              ),
+              key: Key(item.title),
+              background: Container(
+                alignment: Alignment.centerRight,
+                color: Colors.red,
+                child: Icon(
+                  Icons.delete,
+                  color: Colors.white,
+                ),
+              ),
+              onDismissed: (direction) {
+                if (direction == DismissDirection.startToEnd) {
+                  remove(index);
+                }
+              },
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          add();
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+}
